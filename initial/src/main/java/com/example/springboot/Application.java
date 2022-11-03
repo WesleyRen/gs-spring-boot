d4a746572afaa2bd96c31ec967f8a5eab668f9f8
Wesley Ren <wren@walmartlabs.com>
2022-11-03 16:58:48 -0700

    add base code for slow data retrieval

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.example.caching",
+})
 public class Application {
 
 	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
@@ -16,12 +18,17 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
+
+			System.out.println("\nLet's inspect the beans provided by Spring Boot:");
+			System.out.printf("Total Spring Bean count: %s.\n", beanNames.length);
+
+			if (args.length > 1 && args[1].equals("true")) {
+				for (String beanName : beanNames) {
+					System.out.println(beanName);
+				}
 			}
 		};
 	}
