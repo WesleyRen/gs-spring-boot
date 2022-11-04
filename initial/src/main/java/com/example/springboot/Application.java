7d3645f4162d1c6435a629c0013218382062b9f6
Wesley Ren <wren@walmartlabs.com>
2022-11-04 14:52:53 -0700

    add MultiCacheManagerConfig; move all cache config there

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -1,39 +1,21 @@
 package com.example.springboot;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.example.*",
 })
-@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.printf("args length: %d, value: %s\n", args.length, Arrays.stream(args).reduce((ret, v) -> ret + ", " + v));
-	}
-
-	@Bean
-	public Caffeine caffeineConfig() {
-		return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
-	}
-
-	@Bean
-	public CacheManager cacheManager(Caffeine caffeine) {
-		CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-		caffeineCacheManager.setCaffeine(caffeine);
-		return caffeineCacheManager;
 	}
 
 	@Bean
