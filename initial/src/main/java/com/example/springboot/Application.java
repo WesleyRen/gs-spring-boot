b0024d3c52a69821d3da750f095aea44550c9da4
Wesley Ren <wren@walmartlabs.com>
2022-11-03 17:17:22 -0700

    enable default cache

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.example.caching",
 })
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
