859334dcc434f2d645a27a5f4042d21a333e3b53
Wesley Ren <wren@walmartlabs.com>
2022-11-07 17:06:05 -0800

    use newly added debug.level for CacheDemo debug logging

--- initial/src/main/java/com/example/book/CacheDemo.java
+++ initial/src/main/java/com/example/book/CacheDemo.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class CacheDemo implements CommandLineRunner {
@@ -16,33 +18,34 @@ public class CacheDemo implements CommandLineRunner {
   private static final Logger logger = LoggerFactory.getLogger(CacheDemo.class);
 
   private final BookLookupService bookLookupService;
+  private final Environment env;
 
   @Autowired
-  public CacheDemo(BookLookupService bookLookup) {
+  public CacheDemo(BookLookupService bookLookup, Environment env) {
     this.bookLookupService = bookLookup;
+    this.env = env;
   }
 
   @Override
   public void run(String... args) {
-    if (args.length > 0 && args[0].equals("false")) {
-      return;
-    }
-    String a = "isbn-1234";
-    String b = "isbn-4567";
-    List<String> bookList = new ArrayList<>(Arrays.asList(a, b, a, b, a, a));
-    bookList.forEach(book -> {
+    if (Integer.parseInt(Objects.requireNonNull(env.getProperty("debug.level"))) >= 1) {
+      String a = "isbn-1234";
+      String b = "isbn-4567";
+      List<String> bookList = new ArrayList<>(Arrays.asList(a, b, a, b, a, a));
+      bookList.forEach(book -> {
+        long startTime = System.nanoTime();
+        logger.info("fetching " + book + " ...");
+        logger.info("         -> " + bookLookupService.getByIsbn(book).toString() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
+      });
+
       long startTime = System.nanoTime();
-      logger.info("fetching " + book + " ...");
-      logger.info("         -> " + bookLookupService.getByIsbn(book).toString() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
-    });
+      logger.info("fetching all books ...");
+      logger.info("         -> " + bookLookupService.getAll().size() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
 
-    long startTime = System.nanoTime();
-    logger.info("fetching all books ...");
-    logger.info("         -> " + bookLookupService.getAll().size() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
-
-    startTime = System.nanoTime();
-    logger.info("fetching all books again ...");
-    logger.info("         -> " + bookLookupService.getAll().size() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
+      startTime = System.nanoTime();
+      logger.info("fetching all books again ...");
+      logger.info("         -> " + bookLookupService.getAll().size() + ". Time (ms): " + (System.nanoTime() - startTime) / 1000000);
+    }
   }
 
 }
